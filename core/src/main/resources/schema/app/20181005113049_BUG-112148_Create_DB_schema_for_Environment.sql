d4de03817b42e715b2acc49f5964d75107f64272
Ferenc Schneider <fschneider@hortonworks.com>
2018-10-10 13:56:34 +0200

    remove owner from the new domain objs, using more indicies

--- core/src/main/resources/schema/app/20181005113049_BUG-112148_Create_DB_schema_for_Environment.sql
+++ core/src/main/resources/schema/app/20181005113049_BUG-112148_Create_DB_schema_for_Environment.sql
@@ -8,65 +8,52 @@ CREATE TABLE IF NOT EXISTS environment (
     cloudplatform character varying(255) NOT NULL,
     credential_id bigint NOT NULL,
     workspace_id bigint NOT NULL,
-    regions TEXT NOT NULL,
-    owner character varying(255)
+    regions TEXT NOT NULL
 );
-
-ALTER TABLE ONLY environment
-    ADD CONSTRAINT environment_pkey PRIMARY KEY (id);
-
-ALTER TABLE ONLY environment
-    ADD CONSTRAINT uk_environment_workspace_name UNIQUE (workspace_id, name);
-
+ALTER TABLE ONLY environment ADD CONSTRAINT environment_pkey PRIMARY KEY (id);
+ALTER TABLE ONLY environment ADD CONSTRAINT uk_environment_workspace_name UNIQUE (workspace_id, name);
 CREATE INDEX IF NOT EXISTS idx_environment_workspace_id_name ON environment (workspace_id, name);
 
 CREATE TABLE IF NOT EXISTS env_ldap (
     envid bigint NOT NULL,
     ldapid bigint NOT NULL
 );
-
 ALTER TABLE ONLY env_ldap ADD CONSTRAINT fk_env_ldap_envid FOREIGN KEY (envid) REFERENCES environment(id);
-
 ALTER TABLE ONLY env_ldap ADD CONSTRAINT fk_env_ldap_ldapid FOREIGN KEY (ldapid) REFERENCES ldapconfig(id);
-
 ALTER TABLE ONLY env_ldap ADD CONSTRAINT uk_env_ldap_envid_ldapid UNIQUE (envid, ldapid);
-
 CREATE INDEX IF NOT EXISTS idx_env_ldap_envid ON env_ldap (envid);
-
 CREATE INDEX IF NOT EXISTS idx_env_ldap_ldapid ON env_ldap (ldapid);
 
 CREATE TABLE IF NOT EXISTS env_proxy (
     envid bigint NOT NULL,
     proxyid bigint NOT NULL
 );
-
 ALTER TABLE ONLY env_proxy ADD CONSTRAINT fk_env_proxy_envid FOREIGN KEY (envid) REFERENCES environment(id);
-
 ALTER TABLE ONLY env_proxy ADD CONSTRAINT fk_env_proxy_proxyid FOREIGN KEY (proxyid) REFERENCES proxyconfig(id);
-
 ALTER TABLE ONLY env_proxy ADD CONSTRAINT uk_env_proxy_envid_proxyid UNIQUE (envid, proxyid);
-
 CREATE INDEX IF NOT EXISTS idx_env_proxy_envid ON env_proxy (envid);
-
 CREATE INDEX IF NOT EXISTS idx_env_proxy_proxyid ON env_proxy (proxyid);
 
 CREATE TABLE IF NOT EXISTS env_rds (
     envid bigint NOT NULL,
     rdsid bigint NOT NULL
 );
-
 ALTER TABLE ONLY env_rds ADD CONSTRAINT fk_env_rds_envid FOREIGN KEY (envid) REFERENCES environment(id);
-
 ALTER TABLE ONLY env_rds ADD CONSTRAINT fk_env_rds_rdsid FOREIGN KEY (rdsid) REFERENCES rdsconfig(id);
-
 ALTER TABLE ONLY env_rds ADD CONSTRAINT uk_env_rds_envid_rdsid UNIQUE (envid, rdsid);
-
 CREATE INDEX IF NOT EXISTS idx_env_rds_envid ON env_rds (envid);
-
 CREATE INDEX IF NOT EXISTS idx_env_rds_rdsid ON env_rds (rdsid);
+
+CREATE INDEX IF NOT EXISTS idx_ldapconfig_workspace_id_name ON ldapconfig (workspace_id, name);
+CREATE INDEX IF NOT EXISTS idx_proxyconfig_workspace_id_name ON proxyconfig (workspace_id, name);
+CREATE INDEX IF NOT EXISTS idx_rdsconfig_workspace_id_name ON rdsconfig (workspace_id, name);
 
 -- //@UNDO
 -- SQL to undo the change goes here.
+
+DROP INDEX IF EXISTS idx_rdsconfig_workspace_id_name;
+DROP INDEX IF EXISTS idx_proxyconfig_workspace_id_name;
+DROP INDEX IF EXISTS idx_ldapconfig_workspace_id_name;
 
 DROP INDEX IF EXISTS idx_env_rds_rdsid;
 DROP INDEX IF EXISTS idx_env_rds_envid;
